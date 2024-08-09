e298d04524ed9465fb5311c34bde90e754d2d56e
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-09 12:57:20 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Declare a variable that stores an array of any 3 fictional people
+let fictonial = ["RObin","SPiderman","Btman"]
+
+// Console log the first element of the array (using array name and index)
+
+
+// Console log the last element of the array (using array name and index)
+
+// Console log the last element of the array WITHOUT using the index -- use the array length instead
+
+// Change the second element of the array to be a different fictional character
+
+// Stretch: try ADDING a new fictional character to the array
+
